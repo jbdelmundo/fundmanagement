597b7707ed0f4ea802fbd3a4293f304951d8960a
Benj Del Mundo <jbdelmundo@gmail.co>
2017-06-29 14:45:47 +0800

    Updated selectors

--- resources/views/_active_dept_selector.blade.php
+++ resources/views/_active_dept_selector.blade.php
@@ -1,6 +1,11 @@
 <div class="panel panel-default">
     <div class="panel-body">
     <select id='active_dept_selector' class="form-control">
+        <?php 
+            //get the variables here instead in the controller
+            $active_department_id = session('active_dept_id',1 ) ;
+            $departments = \App\Department::all();
+        ?>
         @foreach($departments as $d)
             <option value={{$d->id}}   @if($active_department_id == $d->id) selected @endif >
             	{{$d->short_name}}
--- resources/views/_active_sem_selector.blade.php
+++ resources/views/_active_sem_selector.blade.php
@@ -1,6 +1,13 @@
 <div class="panel panel-default">
     <div class="panel-body">
     <select id='active_sem_selector' class="form-control">
+
+        <?php 
+            //get the variables here instead in the controller
+            $active_aysem = session('active_aysem',\App\Aysem::current->aysem ) ;
+            $aysems = \App\Aysem::all();
+        ?>
+
         @foreach($aysems as $aysem)
             <option value={{$aysem->aysem}}   @if($active_aysem == $aysem->aysem) selected @endif >
             	{{\App\Aysem::shortName($aysem->aysem)}}
